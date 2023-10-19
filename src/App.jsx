50a50c4d55697614f2c451f6e75c42ea221048ac
Marc Chacon <marcchacon1@outlook.com>
2023-10-19 13:48:41 -0500

    add existing project files prior to the push to Github

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Home from './components/home'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import { Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element= {<Blue />} />
+          <Route path="/red" element= {<Red />} />
+          <Route path="/" element= {<Home />} />
+        </Routes>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
